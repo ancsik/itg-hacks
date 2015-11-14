cf845d408bb2aaae3bc9a21b7547e53df922896b
ancsik <chick.tony@gmail.com>
2015-11-14 12:20:43 -0800

    usbhelper fix

--- usbhelper/usbhelper.sh
+++ usbhelper/usbhelper.sh
@@ -6,17 +6,24 @@ error() { (echo "ERROR: $@" >&2) }
 do_read() { (read -rsp "$1, then press any key"$'\n' -n1 key) }
 
 USB_BUS='/sys/bus/usb/devices'
-GREP='^[0-9][0-9]?-[0-9][0-9]?$'
-usbdevs() { (ls ${USB_BUS} | grep -e "${GREP}") }
+GREP='[0-9][0-9]*-[0-9][0-9]*$'
+usbdevs() { (ls "${USB_BUS}" | grep -e "${GREP}") }
+
+do_read "make sure both memory card ports are open"
+INIT="$(usbdevs)"
+echo "${INIT}"
+
 prompt() {
   do_read "insert [P$1] USB drive"
   ATTEMPTS=5
   while [ ${ATTEMPTS} -gt 0 ]; do
     ATTEMPTS=$((ATTEMPTS - 1))
-    NEW=$(usbdevs | grep -v -F "${INIT}")
-    if [ ${NEW} ]; then
-      if [ $(echo ${NEW} | wc -w) == 1 ]
-      then echo ${NEW} && return 0
+    NEW="$(usbdevs)"
+    echo "${NEW}" >&2
+    NEW="$(echo "${NEW}" | grep -vx -F "${INIT}")"
+    if [ "${NEW}" ]; then
+      if [ $(echo "${NEW}" | wc -w) == 1 ]
+      then echo "${NEW}" && return 0
       else error 'multiple new devices found' && return 1
       fi
     fi
@@ -27,8 +34,8 @@ prompt() {
 bindings() {
   DEV=$(prompt $1)
   [ "${DEV}" ] || return 1
-  BNUM=$(cat "${USB_BUS}/busnum")
-  PNUM=$(cat "${USB_BUS}/devpath")
+  BNUM=$(cat "${USB_BUS}/${DEV}/busnum")
+  PNUM=$(cat "${USB_BUS}/${DEV}/devpath")
   tee -a ${OUT} <<[BINDINGS]
 MemoryCardUsbBusP$1=${BNUM}
 MemoryCardUsbLevelP$1=-1
@@ -36,5 +43,4 @@ MemoryCardUsbPortP$1=${PNUM}
 [BINDINGS]
 }
 
-do_read "make sure both memory card ports are open"
-INIT="$(usbdevs)" && bindings 1 && bindings 2
+bindings 1 && bindings 2
