e0f04c9f32986d3aac879f5e3f3c3ed9a15d24ce
ancsik <chick.tony@gmail.com>
2015-11-28 20:20:28 -0800

    usbhelper understands hubs

--- usbhelper/usbhelper.sh
+++ usbhelper/usbhelper.sh
@@ -6,21 +6,18 @@ error() { (echo "ERROR: $@" >&2) }
 do_read() { (read -rsp "$1, then press any key"$'\n' -n1 key) }
 
 USB_BUS='/sys/bus/usb/devices'
-GREP='[0-9][0-9]*-[0-9][0-9]*$'
-usbdevs() { (ls "${USB_BUS}" | grep -e "${GREP}") }
+REGEX=$(n='[0-9]+'; echo "${n}-${n}(\.${n})*$")
+usbdevs() { (ls "${USB_BUS}" | grep -E "${REGEX}") }
 
 do_read "make sure both memory card ports are open"
 INIT="$(usbdevs)"
-echo "${INIT}"
 
 prompt() {
   do_read "insert [P$1] USB drive"
   ATTEMPTS=5
   while [ ${ATTEMPTS} -gt 0 ]; do
     ATTEMPTS=$((ATTEMPTS - 1))
-    NEW="$(usbdevs)"
-    echo "${NEW}" >&2
-    NEW="$(echo "${NEW}" | grep -vx -F "${INIT}")"
+    NEW="$(usbdevs | grep -vx -F "${INIT}")"
     if [ "${NEW}" ]; then
       if [ $(echo "${NEW}" | wc -w) == 1 ]
       then echo "${NEW}" && return 0
